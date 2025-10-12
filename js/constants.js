e90a6b6c050888f89b58ab9aad778390d0f2ed9e
efootballgeorgia <lukaoeil@gmail.com>
2025-10-12 15:16:06 +0400

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,13 @@
+export const CSS = {
+  ACTIVE: 'active',
+  SHOW: 'show',
+  IS_EDITING: 'is-editing',
+  ROW_UPDATED: 'row-updated',
+};
+
+export const EVENTS = {
+  CLICK: 'click',
+  SUBMIT: 'submit',
+  CHANGE: 'change',
+  KEYDOWN: 'keydown',
+};
