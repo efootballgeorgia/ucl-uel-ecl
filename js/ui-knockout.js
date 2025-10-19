7a1159e3467ab7478798c5f15765abeea71755f5
efootballgeorgia <lukaoeil@gmail.com>
2025-10-20 00:27:28 +0400

    Update ui-knockout.js

--- js/ui-knockout.js
+++ js/ui-knockout.js
@@ -45,7 +45,7 @@ function calculateKnockoutData(sortedTeams, knockoutMatches) {
     const topSeeded = qualifiedTeams.slice(0, seeded);
     const playoffTeams = qualifiedTeams.slice(seeded);
 
-    const kopo_matches = Array.from({ length: playoffTeams.length / 2}, (_, i) => ({
+    const kopo_matches = Array.from({ length: playoffTeams.length / 2 }, (_, i) => ({
         id: `r32-${i}`, homeTeam: playoffTeams[i], awayTeam: playoffTeams[playoffTeams.length - 1 - i],
         data: knockoutData[`r32-${i}`]
     }));
@@ -166,10 +166,4 @@ function renderKnockoutCard(match) {
         ${renderTeam(awayTeam, data?.awayScore, awayWinner, homeWinner)}
         ${renderAdminForm()}
     </div>`;
-
 }
-
-
-
-
-
